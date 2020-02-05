be819818ad8705dadd999c634fc1fefc07d7de78
ahmad <auepior@gmail.com>
2020-02-05 23:08:58 +0200

    function

---
+++ css/add.js
@@ -0,0 +1,35 @@
+
+
+
+
+function createBasketball(q){
+    var q;
+    var qu2;
+    var text;
+    var wr;
+    var q2; 
+
+
+q = prompt("who is your favourite singer?!")
+if (q == 'nancy') {text = ("wow me too.!");
+ window.alert(text);
+}
+
+else { text = "next time choose nancy please!";
+window.alert(text);
+}
+
+qu2 = prompt ("do you love basketball? ")
+if (qu2 == 'yes'){ text="you are in the right place";
+window.alert(text)
+}
+else{ text ="ERROR. go away please. this page meant to be for basketball lovers only";
+window.alert(text)
+} 
+q2 = prompt("how old are you?")
+if (q2 => '18'|| q2< '18'){ text = ("Have Fun");
+window.alert(text)
+}
+return q;
+}
+//document.Write('<h3>' + q + '</h3>')
